3805b15c9476a46a6f505df0de7f53f3e827bc6b
Ryan Ferguson <fergusman@gmail.com>
2017-06-15 14:47:13 -0400

    this will add a single example data point

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Article.create(web_url: "https://query.nytimes.com/gst/fullpage.html?res=9507E0DB113FF934A25752C0A96E9C8B63", lead_paragraph: "The former Golden Gloves champion Sadam Ali's Olympic future is in jeopardy as he awaits the backup sample results from a doping test that came back positive for cathine, a banned stimulant found in some cold medicines.Ali, a 19-year-old lightweight from New York who won the 2006 national Golden Gloves, tested positive at an exhibition in China in November. Jim Millman, the chief executive of USA Boxing, said Ali and five other American boxers had flulike symptoms in China.Penalties could range from a warning to a two-year suspension.", headline: "Lightweight Tests Positive", pub_date: "2008-01-17T00:00:00Z", word_count: 92)
