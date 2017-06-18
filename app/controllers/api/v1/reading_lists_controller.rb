8ab964cc933ae7aad195425ee4c942015ef7b51f
iwpeifer <iwpeifer@gmail.com>
2017-06-18 12:52:48 -0400

    got everything working as intended

--- app/controllers/api/v1/reading_lists_controller.rb
+++ app/controllers/api/v1/reading_lists_controller.rb
@@ -2,11 +2,31 @@ class Api::V1::ReadingListsController < ApplicationController
 
   def index
     reading_lists = ReadingList.all
-    render json: reading_lists
+    render json: reading_lists, include: [:articles]
   end
 
   def show
     render json: ReadingList.find(params[:id])
+  end
+
+  def create
+    # byebug
+    reading_list = ReadingList.create(reading_list_params)
+    params["articles"].each do |article|
+      ReadingListArticle.create(reading_list_id: reading_list["id"], article_id: article["id"])
+    end
+    render json: reading_list, include: [:articles]
+  end
+
+  def destroy
+    reading_list = ReadingList.find(params[:id])
+    reading_list.destroy
+  end
+
+  private
+
+  def reading_list_params
+    params.require(:reading_list).permit(:title, { articles: [:id, :web_url, :lead_paragraph, :headling, :pub_date, :word_count]})
   end
 
 end
